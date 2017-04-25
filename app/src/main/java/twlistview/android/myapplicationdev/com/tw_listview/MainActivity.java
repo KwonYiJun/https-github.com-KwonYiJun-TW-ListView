9224d58803c04ef9f28985b2321b1c5f8254cc0e
KwonYiJun <15031795@myrp.edu.sg>
2017-04-25 12:19:07 +0800

    Initial commit

---
+++ app/src/main/java/twlistview/android/myapplicationdev/com/tw_listview/MainActivity.java
@@ -0,0 +1,34 @@
+package twlistview.android.myapplicationdev.com.tw_listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayList<String> al;
+    ArrayAdapter<String> aa;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = (ListView)findViewById(R. id. lvCourse);
+        tv = (TextView)findViewById(R. id. textView);
+
+        al = new ArrayList<String>();
+        al.add("Year 1");
+        al.add("Year 2");
+        al.add("Year 3");
+
+    }
+}
---
+++ app/src/main/java/twlistview/android/myapplicationdev/com/tw_listview/SecondActivity.java
@@ -0,0 +1,9 @@
+package twlistview.android.myapplicationdev.com.tw_listview;
+
+/**
+ * Created by 15031795 on 25/4/2017.
+ */
+
+public class SecondActivity {
+
+}
